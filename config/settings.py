2c0081816fcbc98274b9f88f9e45b2e263a817be
Alex Pavlov <yukiu217@gmail.com>
2024-07-13 00:34:45 +0600

    DB postgresql

--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+from utils import find_env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_DIR = Path(__file__).resolve() / '.env'
+
+load_dotenv(ENV_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,8 +81,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': find_env('DB_NAME'),
+        'HOST': find_env('DB_HOST'),
+        'PORT': find_env('DB_PORT'),
+        'USER': find_env('DB_USER'),
+        'PASSWORD': find_env('DB_PASSWORD')
     }
 }
 
@@ -105,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Omsk'
 
 USE_I18N = True
 
